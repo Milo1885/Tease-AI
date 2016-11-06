8c4de8df74ffae8614ad88fb440a9e86626361aa
Stefaf <stefaf@users.noreply.github.com>
2016-11-06 09:08:58 +0100

    Scripts CLD File Fix Reworked ScriptCheckedListbox serializing and handling code. Now there should be no need to delete these files any more, when swapping to another personality or changing the script files on runtime.
    Added FileClass and FolderClass to avoid Code-String-Mixing all over the place and to enhance maintainability. Both are dependency subclasses of ClassSessionState to gain access to non activated sessions in future releases.

---
+++ Tease AI/Classes/SessionState.FileClass.vb
@@ -0,0 +1,38 @@
+﻿Partial Class SessionState
+
+	Friend Class FileClass
+		Dim _Session As SessionState
+
+		Sub New(session As SessionState)
+			_Session = session
+		End Sub
+
+		Friend ReadOnly Property StartChecklist As String
+			Get
+				Return _Session.Folders.PersonalitySystem & "StartCheckList.cld"
+			End Get
+		End Property
+
+		Friend ReadOnly Property ModuleChecklist As String
+			Get
+				Return _Session.Folders.PersonalitySystem & "ModuleCheckList.cld"
+			End Get
+		End Property
+
+		Friend ReadOnly Property LinkChecklist As String
+			Get
+				Return _Session.Folders.PersonalitySystem & "LinkCheckList.cld"
+			End Get
+		End Property
+
+		Friend ReadOnly Property EndChecklist As String
+			Get
+				Return _Session.Folders.PersonalitySystem & "EndCheckList.cld"
+			End Get
+		End Property
+
+
+	End Class
+
+
+End Class
